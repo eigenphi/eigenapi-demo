ad7898508b8ea6a7263932985970809351c21481
David <hitdavid@gmail.com>
2023-02-28 12:45:30 +0800

    update client version to 0.2.1

--- statistics_data.py
+++ statistics_data.py
@@ -3,17 +3,16 @@ from eigenapi_client.Client import Client
 import APIKeys
 
 
-def get_pool_sandwich():
+def get_pool_sandwich(client):
     '''
     Get the statistics data of pools sandwiched within 30 days.
     :return:
     '''
-    api = Client(apikey)
-    pool_sandwiched_list = api.pool_sandwiched(chain='ethereum')
+    pool_sandwiched_list = client.pool_sandwiched(chain='ethereum', duration=7)
     for pool in pool_sandwiched_list:
         print(pool.address, pool.symbol, pool.sandwichedTrades, pool.sandwichedVolume, pool.trades)
 
 
 if __name__ == '__main__':
-    apikey = APIKeys.APIKEY
-    get_pool_sandwich()
+    client = Client(APIKeys.APIKEY)
+    get_pool_sandwich(client)
--- transactions_query.py
+++ transactions_query.py
@@ -1,81 +1,48 @@
 #!/usr/bin/env python3
-#!/usr/bin/env python3
 import datetime
 import time
-from threading import Thread
 
 from eigenapi_client.Client import Client
-from eigenapi_client.endpoints.schema import Transaction
 
 import APIKeys
 
 
-def latest_block():
-    client = Client(apikey)
+def latest_block(client):
+
     eth_latest = client.block_latest('ethereum')
     print(f"ethereum latest_block is:{eth_latest.blockNumber} blockTimestamp:{eth_latest.blockTimestamp}")
-    time.sleep(1)
     bsc_latest = client.block_latest('bsc')
     print(f"bsc latest_block is:{bsc_latest.blockNumber} blockTimestamp:{bsc_latest.blockTimestamp}")
 
 
-def loop_for_get_transaction():
+def loop_to_get_transaction(client):
     '''
     keeping loop to obtain transaction data,
     If you want to get the data in time, it is recommended to use websocket.
     :return:
     '''
-    client = Client(apikey)
     # First, get the latest block
     latest_block = client.block_latest('ethereum')
     time.sleep(1)
 
-    # set the end time for the query
-    end_time = latest_block.blockTimestamp
-    # set the start time as needed,for example 5 Minute
-    start_time = end_time - 60 * 5
+    # set the end time for the query, latest_block - 1 hour
+    end_time = latest_block.blockTimestamp - 3600
+    # set the start time to end - 1 hour
+    start_time = end_time - 3600
+    transactions = []
     while True:
-        generator = client.transactions('ethereum', start=start_time, end=end_time)
-        for row in generator:
-            for tx in row:
-                # do your code here,for example save transaction to db etc.
-                print(tx.blockNumber, tx.transactionHash)
-
-        time.sleep(1 * 60)
-
-        start_time = end_time
-        # get the latest block again
-        latest_block = client.block_latest('ethereum')
-        end_time = latest_block.blockTimestamp
-        # if latest_block is not updated, do nothing
-        if end_time - start_time < 1 * 60:
-            time.sleep(1 * 60)
-
-
-def get_history_transaction():
-    '''
-    Obtain historical data within a certain time range
-    Support The data starts from 2023-01-01.
-    :return:
-    '''
-    api = Client(apikey)
-
-    start_datetime_object = datetime.datetime.strptime('2023-02-22 21:00:00', '%Y-%m-%d %H:%M:%S')
-    start_time = int(time.mktime(start_datetime_object.timetuple()))
-
-    end_datetime_object = datetime.datetime.strptime('2023-02-22 22:00:00', '%Y-%m-%d %H:%M:%S')
-    end_time = int(time.mktime(end_datetime_object.timetuple()))
-
-
-    generator = api.transactions('ethereum', start=start_time, end=end_time)
-
-    for row in generator:
-        for tx in row:
-            print(tx.blockNumber, tx.transactionHash)
+        result = client.transactions('ethereum', start=start_time, end=end_time)
+        if result is None or len(result) == 0:
+            break
+        transactions.extend(result)
+        first_tx_in_result = result[-1]
+        end_time = first_tx_in_result.blockTimestamp
+        if end_time <= start_time:
+            break
+    print('total transactions count:', len(transactions))
 
 
 if __name__ == '__main__':
-    apikey = APIKeys.APIKEY
-    latest_block()
-    time.sleep(1)
-    loop_for_get_transaction()
+    client = Client(APIKeys.APIKEY, debug=True)
+    latest_block(client)
+    loop_to_get_transaction(client)
